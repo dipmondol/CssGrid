60910788ba587278ea15d2ca036c8afc41dd349d
dipmondol <mondoldip76@gmail.com>
2022-03-10 23:55:53 +0600

    automated navbar

---
+++ advanceNavbar/main.js
@@ -0,0 +1,9 @@
+// Start Toggle Mobile Menu
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileElement = document.querySelector('.nav-mobile');
+
+
+menuToggleOpen.addEventListener('click', () => navMobileElement.classList.add('active'));
+menuToggleClose.addEventListener('click', () => navMobileElement.classList.remove('active'));
+// End Toggle Mobile Menu
