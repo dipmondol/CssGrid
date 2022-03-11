fa27a9343a350b2e1441bbbb5bcce555da81ef0f
dipmondol <mondoldip76@gmail.com>
2022-03-11 23:35:07 +0600

    dark theme

--- advanceNavbar/main.js
+++ advanceNavbar/main.js
@@ -1,9 +1,41 @@
 // Start Toggle Mobile Menu
 const menuToggleOpen = document.querySelector('.menu-toggle-open');
 const menuToggleClose = document.querySelector('.menu-toggle-close');
-const navMobileElement = document.querySelector('.nav-mobile');
+const navMobileElement = document.querySelector('.nav-mob');
 
 
 menuToggleOpen.addEventListener('click', () => navMobileElement.classList.add('active'));
 menuToggleClose.addEventListener('click', () => navMobileElement.classList.remove('active'));
 // End Toggle Mobile Menu
+
+//Theme Block 
+
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+if(currentTheme){
+    bodyElement.classList.add('theme-light');
+}
+
+themeToggleBtn.addEventListener('click', () =>{
+    bodyElement.classList.toggle('theme-light');
+    if(bodyElement.classList.contains('theme-light')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }
+    else{
+        localStorage.removeItem('currentTheme', 'themeActive');
+    }
+});
+// Theme Block
+
+// Start Drop Down Menu
+document.addEventListener('click', element =>{
+    const dropdownMenu = document.querySelector('.dropdown');
+    if(element.target.classList.contains('dropdown-btn')){
+        dropdownMenu.classList.add('active');
+    }else{
+        dropdownMenu.classList.remove('active');
+    }
+});
+
+// End Drop Down Menu
